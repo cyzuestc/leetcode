72f68cb9e5ecbc917ef6e4cfdb40fe5100815340
Administrator <421248827@qq.com>
2018-01-23 17:20:38 +0800

    test

--- sentence_reverse.java
+++ sentence_reverse.java
@@ -8,7 +8,7 @@ public class sentence_reverse {
         String[] res =  s.split(" ");
         for (int i = res.length-1; i >= 0; i--) {
             sb.append(res[i]);
-            sb.append(" 1");
+            sb.append(" ");
         }
     }
 }

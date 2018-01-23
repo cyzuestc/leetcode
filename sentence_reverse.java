843c680f924d605a694ebbc95c7b278ad98cd287
Administrator <421248827@qq.com>
2018-01-23 17:19:31 +0800

    test

--- sentence_reverse.java
+++ sentence_reverse.java
@@ -8,8 +8,7 @@ public class sentence_reverse {
         String[] res =  s.split(" ");
         for (int i = res.length-1; i >= 0; i--) {
             sb.append(res[i]);
-            sb.append(" ");
+            sb.append(" 1");
         }
-        System.out.printf(sb.substring(0,sb.length()-1).toString());
     }
 }
